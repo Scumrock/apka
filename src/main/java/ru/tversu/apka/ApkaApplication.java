adadbf8c822f9ce1b442d0f896f48e0748353290
rgordeev <roma.gordeev@gmail.com>
2022-10-05 14:47:27 +0300

    initial app stub

---
+++ src/main/java/ru/tversu/apka/ApkaApplication.java
@@ -0,0 +1,13 @@
+package ru.tversu.apka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApkaApplication.class, args);
+	}
+
+}
